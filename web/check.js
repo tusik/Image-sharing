eb3f766eba8026425d0f56bf60a6cd6fdbf47c8e
image <onenookone@gmail.com>
2016-10-05 15:58:28 +0800

    v0.0.9BETA可用版

--- web/check.js
+++ web/check.js
@@ -11,7 +11,7 @@ function fileChange(target) {
     }
     var name=target.value;
     var fileName = name.substring(name.lastIndexOf(".")+1).toLowerCase();
-    if(fileName !="jpge" && fileName !="jpg"&& fileName !="png"&& fileName !="bmp"&& fileName !="ico"){
+    if(fileName !="jpge" && fileName !="jpg"&& fileName !="png"&& fileName !="bmp"&& fileName !="ico"&& fileName !="gif"){
         alert("请选择图片文件上传！");
         target.value="";
         return
